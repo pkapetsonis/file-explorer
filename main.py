35d4c0cb6d6992946ad0f07c1393dfb17946ddd2
pkapetsonis <k.panagiotis09@gmail.com>
2021-11-12 16:01:59 +0200

    1.1.0 can open directories and can run programm from other directories Now you can open directories and see their files also you can run the programm from other directories and added that you can run the programm with "./main.py" this method will work only if you run the command  "chmod +x main.py".

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 import os
 import tkinter as tk
 from tkinter.ttk import *
@@ -7,20 +7,44 @@ from PIL import ImageTk
 working_path = os.getcwd()
 files = os.listdir(working_path)
 
+program_path = os.path.dirname(__file__)
 print(working_path)
 print(files)
 
 ICONS_SIZE = (48, 48)
 #---------tk----------
 
+
+
 class DirectoryComponent(tk.Frame):
 
-    def __init__(self, master, filename, filesize, selected_photo):
+    def __init__(self, master, filename, file_path, working_path):
         super().__init__(master, bg="grey", highlightbackground='black', highlightthickness=3)
+        
+        
+        size = os.path.getsize(file_path)
+        size_type = 'Bytes'
+
+        if size > 1024:
+            size = size / 1024
+            size_type = 'kb'
+        if size > 1024:
+            size = size / 1024
+            size_type = 'mb'
+
+        self.filesize = '{:.1f}:{}'.format(size, size_type)
+        selected_photo = icon_file
+
+        if os.path.isfile(filename):
+            selected_photo = icon_file
+    
+        elif os.path.isdir(filename):
+            selected_photo = icon_folder
+            self.filesize = ''
+       
         self.filename = filename 
-        self.filesize = filesize
-        #self.filetype = filetype
         self.photo = selected_photo
+        self.file_path = file_path
         self.make_file_ui()
     
     def make_file_ui(self):
@@ -36,54 +60,68 @@ class DirectoryComponent(tk.Frame):
 
         #self.type_label = tk.Label(self, text=self.filetype, highlightthickness=3, highlightbackground="orange", bg='grey', fg='black', font=('Consolas', 15))
         #self.type_label.pack(side=tk.RIGHT)
+    
 
     def onclick(self, event):
         print(self.filename, 'clicked')
+        if os.path.isdir(self.filename):
+            clicked_folder_path = self.file_path
+            print("new path: ", clicked_folder_path)
+            os.chdir(clicked_folder_path)
+            refresher(self.filename, self.file_path, clicked_folder_path)
+
+     
+           
+            
+def refresher(filename, file_path, clicked_folder_path):
+    global window
+    
+    files = os.listdir(clicked_folder_path)
+    
+    # window.destroy()
+    # print(files)
+    # window = tk.Tk()
+    # window.geometry("500x500")
+    # window.config(bg='#919191')   
+
+    for w in window.winfo_children():
+        w.destroy()
+
+
+    for filename in files:
+        new_file_path = os.path.join(clicked_folder_path, filename)
+        file_label1 = DirectoryComponent(window, filename, new_file_path, working_path)
+        file_label1.pack(fill=tk.X, pady=3)
+    
+    return window
+
+
+            
 
 
 window = tk.Tk()
 window.geometry("500x500")
-window.config(bg='#919191')
+window.config(bg='#919191') 
 #files = [f'test_file{i}' for i in range(5)]
 
 
-img = Image.open(r'assets/file_image.png')
+img = Image.open(os.path.join(program_path, 'assets/file_image.png'))
 img = img.resize(ICONS_SIZE, Image.ANTIALIAS)
 icon_file = ImageTk.PhotoImage(img)
 
-img = Image.open(r'assets/folder_image.png')
+img = Image.open(os.path.join(program_path, r'assets/folder_image.png'))
 img = img.resize(ICONS_SIZE, Image.ANTIALIAS)
 icon_folder = ImageTk.PhotoImage(img)
 
+   
 for filename in files:
+    
     file_path = '{}/{}'.format(working_path, filename)
 
-    size = os.path.getsize(file_path)
-    size_type = 'Bytes'
 
-    if size > 1024:
-         size = size / 1024
-         size_type = 'kb'
-         if size > 1024:
-             size = size / 1024
-             size_type = 'mb'
-
-    filesize = '{:.1f}:{}'.format(size, size_type)
-
-    if os.path.isfile(filename):
-        selected_photo = icon_file
-    
-    elif os.path.isdir(filename):
-        selected_photo = icon_folder
-
-
-    
-
-
-    file_label1 = DirectoryComponent(window, filename, filesize, selected_photo)
+    file_label1 = DirectoryComponent(window, filename, file_path, working_path)
     file_label1.pack(fill=tk.X, pady=3)
 
-
-
+        
 
 window.mainloop()
